bb79663c6fd7dd3e5f9ae82143b3d6e0d23cf165
Steven Hobbs <shobbs@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-04-22 16:54:50 -0600

    kestrel troubleshooting and more feature engineering

---
+++ run_5_daily_flights_LSTM_CPU.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=LSTM_script
+#SBATCH --output=kestrel_lstm_results_%j.txt
+# #SBATCH --partition=standard   # Use a standard CPU partition, adjust the name as needed
+#SBATCH --mem=32GB             
+#SBATCH --time=01:00:00
+#SBATCH --account=athena2
+#SBATCH --cpus-per-task=8    
+
+# navigate to project directory
+cd ~/dfw/predicting_flight_traffic
+
+# activate python environment and run script
+pipenv run python3 5.DFW_daily_flights_LSTM.py
